0d0bcffef7f3eb29295d191650cc7faf26c64043
kadesse <kadesse3@gmail.com>
2026-09-17 18:50:24 +0000

    Correction compatibilite JVM Java Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,12 +19,12 @@ android {
 
     buildFeatures {
         compose = true
-    }
+    }compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
-
+   
     kotlinOptions {
         jvmTarget = "17"
     }
